8864df0f5a59010ac676e470b360a5ad2f92e753
zavvdev <zaviryukha.igor99@gmail.com>
2025-02-02 12:06:11 +0200

    desctructuring

---
+++ examples/desctructuring/array.js
@@ -0,0 +1,7 @@
+function bc__main() {
+  var vec = [1, 2, 3];
+  var [a, b, c] = vec;
+  return a + b + c;
+}
+
+bc__main();
---
+++ examples/desctructuring/object.js
@@ -0,0 +1,7 @@
+function bc__main() {
+  var vec = [1, 2, 3];
+  var { 0: a, 1: b, 2: c } = vec;
+  return a + b + c;
+}
+
+bc__main();
