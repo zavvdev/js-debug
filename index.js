6aa824910c91d6f867bbc9a8ce3bbd48fe4e7b44
zavvdev <zaviryukha.igor99@gmail.com>
2025-10-31 19:54:45 +0000

    js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var k = 42;
+// Example that shows that strings (as well as any other data type) are not passed by
+// value in V8's internal representation. Instead, they are all passed by reference.
 
-var bc__add = (x) => (y) => x + y;
+var bc__main = () => {
+  var k = "hello";
+    
+  % DebugPrint(k); // prints the address of the string "hello"
 
-bc__add(k)(2);
+  var bc__fn = (x) => {
+    % DebugPrint(x); // prints the same address as above
+    var y = " world";
+    return x + y;
+  }
+
+  bc__fn(k);
+};
+
+bc__main();
