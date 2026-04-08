3540f672769009085a92450154f0b0377a9b4f04
zavvdev <zaviryukha.igor99@gmail.com>
2026-04-08 18:32:56 +0100

    upd

--- v8/bc.sh
+++ v8/bc.sh
@@ -1,8 +1,40 @@
 # debug v8
-# gtime -f "Mem: %MKb\nTime: %e" ~/.jsvu/engines/v8-debug/v8-debug --print-code --code-comments --print-bytecode --print-bytecode-filter="bc__*" --print-opt-source --print-opt --trace-opt --trace-baseline --trace-opt-stats --trace-elements-transitions --trace-deopt-verbose --allow-natives-syntax $1
+# gtime -f "Mem: %MKb\nTime: %e" \
+#     ~/.jsvu/engines/v8-debug/v8-debug \
+#     --print-code \
+#     --code-comments \
+#     --print-bytecode \
+#     --print-bytecode-filter="bc__*" \
+#     --print-opt-source \
+#     --print-opt \
+#     --trace-opt \
+#     --trace-baseline \
+#     --trace-opt-stats \
+#     --trace-elements-transitions \
+#     --trace-deopt-verbose \
+#     --allow-natives-syntax \
+#     "$1"
 
 # raw v8
-# gtime -f "Mem: %MKb\nTime: %e" ~/.jsvu/engines/v8/v8 --print-bytecode --print-bytecode-filter="bc__*" --print-opt-source --print-opt --trace-opt --trace-baseline --trace-opt-stats --trace-opt-verbose --trace-deopt-verbose --allow-natives-syntax $1
+# gtime -f "Mem: %MKb\nTime: %e" \
+#     ~/.jsvu/engines/v8/v8 \
+#     --print-bytecode \
+#     --print-bytecode-filter="bc__*" \
+#     --print-opt-source \
+#     --print-opt \
+#     --trace-opt \
+#     --trace-baseline \
+#     --trace-opt-stats \
+#     --trace-opt-verbose \
+#     --trace-deopt-verbose \
+#     --allow-natives-syntax \
+#     "$1"
 
 # debug shell
-gtime -f "Mem: %MKb\nTime: %e" ~/.jsvu/engines/v8-debug/v8-debug --print-bytecode --print-bytecode-filter="bc__*" --allow-natives-syntax --shell $1
+gtime -f "Mem: %MKb\nTime: %e" \
+  ~/.jsvu/engines/v8-debug/v8-debug \
+  --print-bytecode \
+  --print-bytecode-filter="bc__*" \
+  --allow-natives-syntax \
+  --shell \
+  "$1"
