e4a909889232dd839bcaf5a00d8b75393d1fb0b8
zavvdev <zaviryukha.igor99@gmail.com>
2026-04-15 19:49:45 +0100

    upd

--- examples/env-record-capture/dont-capture-if-not-used.js
+++ examples/env-record-capture/dont-capture-if-not-used.js
@@ -2,6 +2,18 @@ var getClos = () => {
   var bigArr = new Array(1000);
   var x = 1;
 
+  // bigArr won't be included into returned function
+  // outer environment because it's not used there.
+  // Specification says that all identifiers should
+  // be captured but this is completely up to JS
+  // runtime engine to decide what to do with unused
+  // identifiers. V8 does not capture them.
+
+  // We may want to do something like this in order
+  // to make sure that bigArr won't be captured
+  // and memory is freed.
+  // bigArr = null;
+
   return () => {
     console.log(x);
   }
